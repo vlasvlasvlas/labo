bf55f48feab5b42183ced9668698f0ba3a15d53a
Laboratorio de Implementacion <88951773+labo-imp@users.noreply.github.com>
2022-03-20 08:47:54 -0300

    z252_aplicar_rpart_loss_kaggle.r

---
+++ src/rpart/hidden/z252_aplicar_rpart_loss_kaggle.r
@@ -0,0 +1,64 @@
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+#cargo las librerias que necesito
+require("data.table")
+require("rpart")
+require("rpart.plot")
+
+#Aqui debe cambiar los parametros por los que desea probar
+
+param_basicos  <- list( "cp"=          -0.5,  #complejidad minima
+                        "minsplit"=   80,     #minima cantidad de registros en un nodo para hacer el split
+                        "minbucket"=   5,     #minima cantidad de registros en una hoja
+                        "maxdepth"=    8 )    #profundidad máxima del arbol
+
+
+#Aqui se debe poner la carpeta de SU computadora local
+setwd("D:\\gdrive\\Austral2022R\\")  #Establezco el Working Directory
+
+#cargo los datos de 202011 que es donde voy a ENTRENAR el modelo
+dtrain  <- fread("./datasets/paquete_premium_202011.csv")
+
+#genero el modelo,  aqui se construye el arbol
+modelo  <- rpart("clase_ternaria ~ .",  #quiero predecir clase_ternaria a partir de el resto de las variables
+                 data = dtrain,
+                 xval=0,
+                 cp=        -0.3,   #esto significa no limitar la complejidad de los splits
+                 minsplit=  80,     #minima cantidad de registros para que se haga el split
+                 minbucket=  1,     #tamaño minimo de una hoja
+                 maxdepth=   4 )    #profundidad maxima del arbol
+
+
+#grafico el arbol
+prp(modelo, extra=101, digits=5, branch=1, type=4, varlen=0, faclen=0)
+
+
+#Ahora aplico al modelo  a los datos de 202101  y genero la salida para kaggle
+
+#cargo los datos de 202011, que es donde voy a APLICAR el modelo
+dapply  <- fread("./datasets/paquete_premium_202101.csv")
+
+#aplico el modelo a los datos nuevos
+prediccion  <- predict( modelo, dapply , type = "prob")
+
+#prediccion es una matriz con TRES columnas, llamadas "BAJA+1", "BAJA+2"  y "CONTINUA"
+#cada columna es el vector de probabilidades 
+
+#agrego a dapply una columna nueva que es la probabilidad de BAJA+2
+dapply[ , prob_baja2 := prediccion[, "BAJA+2"] ]
+
+#solo le envio estimulo a los registros con probabilidad de BAJA+2 mayor  a  1/60
+dapply[ , Predicted  := as.numeric(prob_baja2 > 1/60) ]
+
+#genero un dataset con las dos columnas que me interesan
+entrega  <- dapply[   , list(numero_de_cliente, Predicted) ] #genero la salida
+
+#genero el archivo para Kaggle
+#creo la carpeta donde va el experimento
+dir.create( "./labo/exp/" ) 
+dir.create( "./labo/exp/KA2022/" ) 
+
+fwrite( entrega, 
+        file= "./labo/exp/KA2022/K242_001.csv", 
+        sep= "," )
