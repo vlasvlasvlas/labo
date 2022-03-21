d9cfe852e1ee6aceb80d797a9ff76e1e0002060d
Laboratorio de Implementacion <88951773+labo-imp@users.noreply.github.com>
2022-03-21 09:34:10 -0300

    511_lightgbm_default.r

---
+++ src/lightgbm/511_lightgbm_default.r
@@ -0,0 +1,52 @@
+# LightGBM  totalmente puro, con parametros por default, sin quitar ninguna variable
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+require("data.table")
+require("lightgbm")
+
+#Aqui se debe poner la carpeta de la computadora local
+setwd("D:\\gdrive\\Austral2022R\\")   #Establezco el Working Directory
+
+#cargo el dataset donde voy a entrenar
+dataset  <- fread("./datasets/paquete_premium_202011.csv", stringsAsFactors= TRUE)
+
+
+#paso la clase a binaria que tome valores {0,1}  enteros
+dataset[ , clase01 := ifelse( clase_ternaria=="BAJA+2", 1L, 0L) ]
+
+#los campos que se van a utilizar
+campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01") )
+
+
+#dejo los datos en el formato que necesita LightGBM
+dtrain  <- lgb.Dataset( data= data.matrix(  dataset[ , campos_buenos, with=FALSE]),
+                        label= dataset$clase01 )
+
+#genero el modelo con los parametros por default
+modelo  <- lgb.train( data= dtrain,
+                      param= list( objective= "binary")
+                    )
+
+#aplico el modelo a los datos sin clase
+dapply  <- fread("./datasets/paquete_premium_202101.csv")
+
+#aplico el modelo a los datos nuevos
+prediccion  <- predict( modelo, 
+                        data.matrix( dapply[, campos_buenos, with=FALSE ])                                 )
+
+
+#Genero la entrega para Kaggle
+entrega  <- as.data.table( list( "numero_de_cliente"= dapply[  , numero_de_cliente],
+                                 "Predicted"= prediccion > 1/60)  ) #genero la salida
+
+dir.create( "./labo/exp/",  showWarnings = FALSE ) 
+dir.create( "./labo/exp/KA2511/", showWarnings = FALSE )
+archivo_salida  <- "./labo/exp/KA2511/KA_511_001.csv"
+
+#genero el archivo para Kaggle
+fwrite( entrega, 
+        file= archivo_salida, 
+        sep= "," )
