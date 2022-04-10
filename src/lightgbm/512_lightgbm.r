7e3287334174d6d0dde875a22c109fddd7774e88
vlasvlasvlas <vlasvlasvlas@gmail.com>
2022-04-10 16:01:07 -0300

    novedades

--- src/lightgbm/512_lightgbm.r
+++ src/lightgbm/512_lightgbm.r
@@ -7,11 +7,23 @@ gc()             #garbage collection
 require("data.table")
 require("lightgbm")
 
+# primos
+# 295873
+# 527173
+# 328789
+# 825733
+# 191519
+# 341963
+# 590771
+# 765103
+# 402263
+# 584707
+
 #Aqui se debe poner la carpeta de la computadora local
-setwd("D:\\gdrive\\Austral2022R\\")   #Establezco el Working Directory
+setwd("F:\\labimp_1\\")   #Establezco el Working Directory
 
 #cargo el dataset donde voy a entrenar
-dataset  <- fread("./datasets/paquete_premium_202011.csv", stringsAsFactors= TRUE)
+dataset  <- fread("datasets\\paquete_premium_202011.csv", stringsAsFactors= TRUE)
 
 
 #paso la clase a binaria que tome valores {0,1}  enteros
@@ -27,14 +39,24 @@ dtrain  <- lgb.Dataset( data= data.matrix(  dataset[ , campos_buenos, with=FALSE
 
 #genero el modelo con los parametros por default
 modelo  <- lgb.train( data= dtrain,
-                      param= list( objective=        "binary",
-                                   num_iterations=     50,  #40
-                                   num_leaves=         64,  #64
-                                   min_data_in_leaf= 3000 ) #3000
+                      param= list( objective= "binary",
+                                   metric= "custom",
+                                   first_metric_only= TRUE,
+                                   boost_from_average= TRUE,
+                                   feature_pre_filter= FALSE,
+                                   verbosity= -100,
+                                   seed= 825733,
+                                   max_depth=  -1,
+                                   min_gain_to_split= 0.0,
+                                   lambda_l1= 0.0,
+                                   lambda_l2= 0.0,
+                                   max_bin= 31,
+                                   num_iterations= 9999,
+                                   force_row_wise= TRUE )
                     )
 
 #aplico el modelo a los datos sin clase
-dapply  <- fread("./datasets/paquete_premium_202101.csv")
+dapply  <- fread("datasets\\paquete_premium_202101.csv")
 
 #aplico el modelo a los datos nuevos
 prediccion  <- predict( modelo, 
@@ -45,9 +67,9 @@ prediccion  <- predict( modelo,
 entrega  <- as.data.table( list( "numero_de_cliente"= dapply[  , numero_de_cliente],
                                  "Predicted"= prediccion > 1/60)  ) #genero la salida
 
-dir.create( "./labo/exp/",  showWarnings = FALSE ) 
-dir.create( "./labo/exp/KA2512/", showWarnings = FALSE )
-archivo_salida  <- "./labo/exp/KA2512/KA_512_001.csv"
+dir.create( "labo\\exp\\",  showWarnings = FALSE ) 
+dir.create( "labo\\exp\\KA2512/", showWarnings = FALSE )
+archivo_salida  <- "labo\\exp\\KA2512/KA_512_002.csv"
 
 #genero el archivo para Kaggle
 fwrite( entrega, 
---
+++ src/xgboost/vb_553_xgboost_stump_highest.r
@@ -0,0 +1,74 @@
+# XGBoost  sabor tradicional
+# ensemble de 400 arboles de APENAS altura 1  (decision stumps), la raiz y dos hojas
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+require("data.table")
+require("xgboost")
+
+#Aqui se debe poner la carpeta de la computadora local
+setwd("F:\\labimp_1\\")   #Establezco el Working Directory
+
+#cargo el dataset donde voy a entrenar
+dataset  <- fread("datasets\\paquete_premium_202011.csv", stringsAsFactors= TRUE)
+
+
+# agrego columnas
+
+
+
+#paso la clase a binaria que tome valores {0,1}  enteros
+dataset[ , clase01 := ifelse( clase_ternaria=="BAJA+2", 1L, 0L) ]
+
+#los campos que se van a utilizar
+campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01") )
+
+
+#dejo los datos en el formato que necesita XGBoost
+dtrain  <- xgb.DMatrix( data= data.matrix(  dataset[ , campos_buenos, with=FALSE]),
+                        label= dataset$clase01 )
+
+#genero el modelo con los parametros por default
+modelo  <- xgb.train( data= dtrain,
+                      param= list( 
+                                   objective=       "binary:logistic",
+                                   max_depth=           1,    #arboles de altura 1, solo dos hojas ! si lo cambio empeora
+                                   min_child_weight=   12,
+                                   
+                                   #test (probando otros parametros de internet, falta revisar, podriamos hacer una búsqueda optimización de estos params para xgboost?)
+								   # busar mas opciones
+                                   tree_method = "hist",
+                                   grow_policy="lossguide",
+                                   learning_rate = 0.05, 
+                                   top_rate = 0.5, 
+                                   other_rate = 0.1, 
+                                   feature_fraction_bynode = 0.2, 
+                                   boosting_type = "goss",
+                                   eta=0.03
+                                   
+                    ),
+                                   nrounds= 2500 #se subio el valor de nrounds
+                    )
+
+#aplico el modelo a los datos sin clase
+dapply  <- fread("datasets\\paquete_premium_202101.csv")
+
+#aplico el modelo a los datos nuevos
+prediccion  <- predict( modelo, 
+                        data.matrix( dapply[, campos_buenos, with=FALSE ]) )
+
+
+#Genero la entrega para Kaggle
+entrega  <- as.data.table( list( "numero_de_cliente"= dapply[  , numero_de_cliente],
+                                 "Predicted"= prediccion > 1/60)  ) #genero la salida
+
+dir.create( "labo\\exp\\",  showWarnings = FALSE ) 
+dir.create( "labo\\exp\\KA2553/", showWarnings = FALSE )
+archivo_salida  <- "labo\\exp\\KA2553/KA_553_ok_02.csv"
+
+#genero el archivo para Kaggle
+fwrite( entrega, 
+        file= archivo_salida, 
+        sep= "," )
