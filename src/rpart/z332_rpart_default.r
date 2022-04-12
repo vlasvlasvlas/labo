d2be09419a29f71f243e0ffeba271d1b977095ea
Laboratorio de Implementacion <88951773+labo-imp@users.noreply.github.com>
2022-04-12 01:13:20 -0300

    z332_rpart_default.r

---
+++ src/rpart/z332_rpart_default.r
@@ -0,0 +1,170 @@
+#Optimizacion Bayesiana de hiperparametros de  rpart
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+require("data.table")
+require("rlist")
+
+require("rpart")
+require("parallel")
+
+
+hs  <- makeParamSet(
+          makeNumericParam("cp"       , lower= -1   , upper=    0.1),
+          makeIntegerParam("minsplit" , lower=  1L  , upper= 8000L),  #la letra L al final significa ENTERO
+          makeIntegerParam("minbucket", lower=  1L  , upper= 2000L),
+          makeIntegerParam("maxdepth" , lower=  3L  , upper=   20L),
+          forbidden = quote( minbucket > 0.5*minsplit ) )             # minbuket NO PUEDE ser mayor que la mitad de minsplit
+
+ksemilla_azar  <- 102191   #cambiar por la primer semilla
+
+#------------------------------------------------------------------------------
+#graba a un archivo los componentes de lista
+#para el primer registro, escribe antes los titulos
+
+loguear  <- function( reg, arch=NA, folder="./work/", ext=".txt", verbose=TRUE )
+{
+  archivo  <- arch
+  if( is.na(arch) )  archivo  <- paste0( folder, substitute( reg), ext )
+
+  if( !file.exists( archivo ) )  #Escribo los titulos
+  {
+    linea  <- paste0( "fecha\t", 
+                      paste( list.names(reg), collapse="\t" ), "\n" )
+
+    cat( linea, file=archivo )
+  }
+
+  linea  <- paste0( format(Sys.time(), "%Y%m%d %H%M%S"),  "\t",     #la fecha y hora
+                    gsub( ", ", "\t", toString( reg ) ),  "\n" )
+
+  cat( linea, file=archivo, append=TRUE )  #grabo al archivo
+
+  if( verbose )  cat( linea )   #imprimo por pantalla
+}
+#------------------------------------------------------------------------------
+#particionar agrega una columna llamada fold a un dataset que consiste en una particion estratificada segun agrupa
+# particionar( data=dataset, division=c(70,30), agrupa=clase_ternaria, seed=semilla)   crea una particion 70, 30 
+# particionar( data=dataset, division=c(1,1,1,1,1), agrupa=clase_ternaria, seed=semilla)   divide el dataset en 5 particiones
+
+particionar  <- function( data, division, agrupa="", campo="fold", start=1, seed=NA )
+{
+  if( !is.na( seed)  )   set.seed( seed )
+
+  bloque  <- unlist( mapply(  function(x,y) { rep( y, x ) }, division, seq( from=start, length.out=length(division) )  ) )
+
+  data[ , (campo) :=  sample( rep( bloque, ceiling(.N/length(bloque))) )[1:.N],
+           by= agrupa ]
+}
+#------------------------------------------------------------------------------
+#fold_test  tiene el numero de fold que voy a usar para testear, entreno en el resto de los folds
+#param tiene los hiperparametros del arbol
+
+ArbolSimple  <- function( fold_test, data, param )
+{
+  #genero el modelo
+  modelo  <- rpart("clase_ternaria ~ .", 
+                   data= data[ fold != fold_test, ],  #entreno en todo MENOS el fold_test que uso para testing
+                   xval= 0,
+                   control= param )
+
+  #aplico el modelo a los datos de testing
+  prediccion  <- predict( modelo, 
+                          data[ fold==fold_test, ],  #aplico el modelo sobre los datos de testing
+                          type= "prob")   #quiero que me devuelva probabilidades
+
+  prob_baja2  <- prediccion[, "BAJA+2"]  #esta es la probabilidad de baja
+
+  #calculo la ganancia
+  ganancia_testing  <- data[ fold==fold_test ][ prob_baja2 > 1/60,  
+                                                sum( ifelse( clase_ternaria=="BAJA+2", 59000, -1000 ) )] 
+
+  return( ganancia_testing )  #esta es la ganancia sobre el fold de testing, NO esta normalizada
+}
+#------------------------------------------------------------------------------
+
+ArbolesCrossValidation  <- function( data, param, qfolds, pagrupa, semilla )
+{
+  divi  <- rep( 1, qfolds )  # generalmente  c(1, 1, 1, 1, 1 )  cinco unos
+
+  particionar( data, divi, seed=semilla, agrupa=pagrupa )  #particiono en dataset en folds
+
+  ganancias  <- mcmapply( ArbolSimple, 
+                          seq(qfolds), # 1 2 3 4 5
+                          MoreArgs= list( data, param), 
+                          SIMPLIFY= FALSE,
+                          mc.cores= 1 )   #se puede subir a qfolds si posee Linux o Mac OS
+
+  data[ , fold := NULL ]
+
+  #devuelvo la primer ganancia y el promedio
+  ganancia_promedio  <- mean( unlist( ganancias ) )   #promedio las ganancias
+  ganancia_promedio_normalizada  <- ganancia_promedio * qfolds  #aqui normalizo la ganancia
+  
+  return( ganancia_promedio_normalizada )
+}
+#------------------------------------------------------------------------------
+#esta funcion solo puede recibir los parametros que se estan optimizando
+#el resto de los parametros, lamentablemente se pasan como variables globales
+
+EstimarGanancia  <- function( x )
+{
+   GLOBAL_iteracion  <<-  GLOBAL_iteracion + 1
+
+   xval_folds  <- 5
+   ganancia  <- ArbolesCrossValidation( dataset,
+                                        param= x, #los hiperparametros del arbol
+                                        qfolds= xval_folds,  #la cantidad de folds
+                                        pagrupa= "clase_ternaria",
+                                        semilla= ksemilla_azar )
+
+   #logueo 
+   xx  <- x
+   xx$xval_folds  <-  xval_folds
+   xx$ganancia  <- ganancia
+   xx$iteracion <- GLOBAL_iteracion
+   loguear( xx,  arch= archivo_log )
+
+   return( ganancia )
+}
+#------------------------------------------------------------------------------
+#Aqui empieza el programa
+
+setwd( "D:\\gdrive\\Austral2022R\\" )
+
+#cargo el dataset
+dataset  <- fread("./datasets/paquete_premium_202011.csv")   #donde entreno
+
+
+#creo la carpeta donde va el experimento
+# HT  representa  Hiperparameter Tuning
+dir.create( "./labo/exp/",  showWarnings = FALSE ) 
+dir.create( "./labo/exp/HT3220/", showWarnings = FALSE )
+setwd("D:\\gdrive\\Austral2022R\\labo\\exp\\HT3320\\")   #Establezco el Working Directory DEL EXPERIMENTO
+
+
+archivo_log  <- "HT332.txt"
+
+
+#leo si ya existe el log, para retomar en caso que se se corte el programa
+GLOBAL_iteracion  <- 0
+
+if( file.exists(archivo_log) )
+{
+ tabla_log  <- fread( archivo_log )
+ GLOBAL_iteracion  <- nrow( tabla_log )
+}
+
+
+
+#La llamada con los parametros por default
+
+x  <- list(  cp=          0.01,
+             minsplit=   20,
+             minbucket=   6,
+             maxdepth=   30
+           )
+
+EstimarGanancia( x )
