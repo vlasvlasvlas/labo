c1fc87059dea695ac5d85beb50520606c336f55b
Laboratorio de Implementacion <88951773+labo-imp@users.noreply.github.com>
2022-05-08 23:08:54 -0300

    exp_lib.r  prueba Ubuntu 22.04 Training Strategy

--- src/lib/exp_lib.r
+++ src/lib/exp_lib.r
@@ -614,7 +614,7 @@ exp_log  <- function( reg, arch=NA, folder="./", ext=".txt", verbose=TRUE )
 
 #source( "~/labo/src/lib/exp_lib.r" ) 
 #exp_start( "FE8120" )
-#exp_start( "TS8120" )
+#exp_start( "TS8210" )
 #exp_start( "HT8310" )
 
 #exp_start( "FE8120" )
